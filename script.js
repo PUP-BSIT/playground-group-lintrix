70df78670b69f2e88e740e1da7fe52126692625e
mingo_edjudah <edjudah.ej@gmail.com>
2024-03-24 14:33:34 +0800

    copy files

---
+++ script.js
@@ -0,0 +1,8 @@
+var Fruit;
+(function (Fruit) {
+    Fruit["APPLE"] = "apple";
+    Fruit["BANANA"] = "banana";
+    Fruit["ORANGE"] = "orange";
+    Fruit["GRAPE"] = "grape";
+    Fruit["WATERMELON"] = "watermelon";
+})(Fruit || (Fruit = {}));
